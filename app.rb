1df776db7f3d6e5622f34a994df79a76fba6ae12
muntasir2165 <muntasir2165@hotmail.com>
2017-06-28 18:00:34 +0000

    implemented the random word app using service oriented architecture

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require 'sinatra'
 
 class MyApp < Sinatra::Base
+  # return a random word to the client
   get '/' do
-    "<!DOCTYPE html><html><head></head><body><h1>Hello World</h1></body></html>"
+    file = "/usr/share/dict/words" #a file of words
+    maxNumber = `echo -n $(cat -n #{file} | wc -l)`.to_i
+    randomNumber = Random.new.rand(1..maxNumber) 
+    randomWord = `ruby -ne '(print $_ and exit) if $.==#{randomNumber}' /usr/share/dict/words`.downcase
+    "<!DOCTYPE html><html><head></head><body style=\"text-align: center\"><h1>Random Word is:<br><span>#{randomWord}</span></h1></body></html>"
   end
 end
 
